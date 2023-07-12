82bee662f617fd392a9ea69785da4a4a55d294a0
Joshua Wiley <128871208+JoJaWiley@users.noreply.github.com>
2023-07-11 21:09:33 -0500

    Add files via upload

--- src/java/backendfacts/ThreadCategory.java
+++ src/java/backendfacts/ThreadCategory.java
@@ -1,5 +1,22 @@
 package backendfacts;
 
 public class ThreadCategory {
+    private int threadCatID;
+    private String title;
     
+    public int getThreadCatID() {
+        return threadCatID;
+    }
+    
+    public void setThreadCatID(int threadCatID) {
+        this.threadCatID = threadCatID;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
